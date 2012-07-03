e82ae550e5bb926bab9db1b44d7693bf56450539
Grain d'café <cafe.en.grain@gmail.com>
2012-07-03 10:15:14 +0200

    New License : CeCILL-C

--- src/graindcafe/tribu/TribuZombie/ITribuBoss.java
+++ src/graindcafe/tribu/TribuZombie/ITribuBoss.java
@@ -32,87 +32,18 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL-C license and that you accept its terms.
  ******************************************************************************/
-package graindcafe.tribu.Inventory;
+package graindcafe.tribu.TribuZombie;
+
+import java.util.List;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
-public class TribuTempInventory {
-	private ItemStack[] armor = new ItemStack[4];
-	private ItemStack[] inventory = new ItemStack[36];
-	private Player p;
+public interface ITribuBoss {
 
-	public TribuTempInventory(Player p) {
-		this.p = p;
-	}
-
-	public TribuTempInventory(Player p, boolean captureNow) {
-		this.p = p;
-		if (captureNow)
-			capture();
-	}
-
-	public TribuTempInventory(Player p, ItemStack[] items) {
-		this.p = p;
-		add(items);
-	}
-
-	public void add(ItemStack[] items) {
-		if (items.length > 36) {
-			// We have a big problem...
-			// TODO:
-			byte i = 0;
-			while (i < 36) {
-				inventory[i] = items[i];
-				i++;
-			}
-		} else {
-			byte i = 0;
-			for (ItemStack item : items) {
-				inventory[i] = item;
-				i++;
-			}
-
-		}
-	}
-
-	public void capture() {
-		inventory = p.getInventory().getContents();
-		armor = p.getInventory().getArmorContents();
-		p.getInventory().clear();
-		p.getInventory().setArmorContents(null);
-	}
-
-	public void drop(Location dropPlace) {
-		for (ItemStack item : inventory)
-			dropPlace.getWorld().dropItem(dropPlace, item);
-		for (ItemStack item : armor)
-			dropPlace.getWorld().dropItem(dropPlace, item);
-	}
-
-	public void restore() {
-
-		// clear the inventory
-		p.getInventory().clear();
-		p.getInventory().setArmorContents(null);
-		// add items
-		p.getInventory().setContents(inventory);
-		p.getInventory().setArmorContents(armor);
-	}
-
-	@Override
-	public String toString() {
-		String r;
-		r = "Inventory :\n";
-		for (ItemStack item : inventory) {
-			r += item.getType() + "x" + item.getAmount();
-		}
-		r = "Armor :\n";
-		for (ItemStack item : armor) {
-			r += item.getType() + "(" + item.getDurability() + ")";
-		}
-		return r;
-
-	}
+	public List<Player> getPlayers();
+	public double getElapsedTime();
+	public int getHealth();
+	public Location getPositon();
+	
 }
