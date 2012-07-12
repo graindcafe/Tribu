922b23b3dc321e37326ea398f55abd310bd4d982
Grain d'café <cafe.en.grain@gmail.com>
2012-07-12 15:45:25 +0200

    Fixed spam

--- src/graindcafe/tribu/Signs/TollSign.java
+++ src/graindcafe/tribu/Signs/TollSign.java
@@ -53,7 +53,10 @@ public class TollSign extends TribuSign {
 	private int cost;
 	// private boolean clicked = false;
 	private Block linkedButton;
+	// TODO: remove static
 	private static LinkedList<Player> allowedPlayer;
+	private Player lastPlayerTry;
+	private boolean preventSpam = false;
 
 	public TollSign(Tribu plugin, Location pos, String[] lines) {
 		super(plugin, pos);
@@ -74,27 +77,17 @@ public class TollSign extends TribuSign {
 	@Override
 	public void init() {
 		Block current;
-		// plugin.LogInfo("Toll Sign at " + pos.getBlockX() + ", " +
-		// pos.getBlockY() + ", " + pos.getBlockZ());
 		BlockFace[] firstFaces = new BlockFace[] { BlockFace.SELF, BlockFace.UP, BlockFace.DOWN };
 		BlockFace[] secondFaces = new BlockFace[] { BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST };
 		for (BlockFace bf : firstFaces) {
 			current = pos.getBlock().getRelative(bf);
 			for (BlockFace bf2 : secondFaces) {
-				/*
-				 * plugin.LogInfo(bf2.name() + " face to " + bf.name() +
-				 * " face at " +
-				 * current.getRelative(bf2).getLocation().getBlockX() + ", " +
-				 * current.getRelative(bf2).getLocation().getBlockY() + ", " +
-				 * current.getRelative(bf2).getLocation().getBlockZ() + " is " +
-				 * current.getRelative(bf2).getType());
-				 */
-
+				// if it's a clickable block
 				if (current.getRelative(bf2).getType() == Material.LEVER || current.getRelative(bf2).getType() == Material.STONE_BUTTON
 						|| current.getRelative(bf2).getType() == Material.STONE_PLATE || current.getRelative(bf2).getType() == Material.WOOD_PLATE
-						|| current.getRelative(bf2).getType() == Material.WOODEN_DOOR || current.getRelative(bf2).getType() == Material.TRAP_DOOR 
-						) {
+						|| current.getRelative(bf2).getType() == Material.WOODEN_DOOR || current.getRelative(bf2).getType() == Material.TRAP_DOOR) {
 					linkedButton = current.getRelative(bf2);
+					preventSpam = linkedButton.getType() == Material.STONE_PLATE || linkedButton.getType() == Material.WOOD_PLATE;
 					return;
 				}
 			}
@@ -113,26 +106,25 @@ public class TollSign extends TribuSign {
 		// Wait for the second event of a button
 
 		if (linkedButton != null) {
-
 			if (e.getClickedBlock().equals(linkedButton)) {
 				Player p = e.getPlayer();
-				
-				PlayerStats stats = plugin.getStats(p);
-				if (!getAllowedPlayer().contains(p) && !stats.subtractmoney(cost)) {
-					
-					Tribu.messagePlayer(p,plugin.getLocale("Message.YouDontHaveEnoughMoney"));
-					e.setCancelled(true);
-					if(linkedButton.getType() == Material.WOODEN_DOOR)
-					{
-						Door d=new Door(linkedButton.getData());
-						d.setOpen(!d.isOpen());
-						linkedButton.setData(d.getData());	
+				if (!preventSpam || !lastPlayerTry.equals(p)) {
+					PlayerStats stats = plugin.getStats(p);
+					if (!getAllowedPlayer().contains(p) && !stats.subtractmoney(cost)) {
+
+						Tribu.messagePlayer(p, plugin.getLocale("Message.YouDontHaveEnoughMoney"));
+						e.setCancelled(true);
+						if (linkedButton.getType() == Material.WOODEN_DOOR) {
+							Door d = new Door(linkedButton.getData());
+							d.setOpen(!d.isOpen());
+							linkedButton.setData(d.getData());
+						}
+					} else {
+						getAllowedPlayer().add(p);
+						Tribu.messagePlayer(p, String.format(plugin.getLocale("Message.PurchaseSuccessfulMoney"), String.valueOf(stats.getMoney())));
 					}
-				} else
-				{
-					getAllowedPlayer().add(p);
-					Tribu.messagePlayer(p,String.format(plugin.getLocale("Message.PurchaseSuccessfulMoney"), String.valueOf(stats.getMoney())));
 				}
+				lastPlayerTry = p;
 			}
 		}
 
